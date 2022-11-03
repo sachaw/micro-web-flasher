8d8deab677af0f79d44a80ceb3dba9e423518085
Sacha Weatherstone <sachaw100@hotmail.com>
2022-11-03 22:20:14 +1000

    init

---
+++ src/targets/base.ts
@@ -0,0 +1,54 @@
+import { ESPLoader } from '../ESPLoader.js';
+
+export abstract class BaseDevice {
+  public abstract CHIP_NAME: string;
+  public abstract IMAGE_CHIP_ID: number;
+  public abstract EFUSE_RD_REG_BASE: number;
+  public abstract DR_REG_SYSCON_BASE: number;
+  public abstract UART_CLKDIV_REG: number;
+  public abstract UART_CLKDIV_MASK: number;
+  public abstract UART_DATE_REG_ADDR: number;
+  public abstract XTAL_CLK_DIVIDER: number;
+  public abstract FLASH_WRITE_SIZE: number;
+  public abstract BOOTLOADER_FLASH_OFFSET: number;
+  public abstract FLASH_SIZES: Map<string, number>;
+  public abstract SPI_REG_BASE: number;
+  public abstract SPI_USR_OFFS: number;
+  public abstract SPI_USR1_OFFS: number;
+  public abstract SPI_USR2_OFFS: number;
+  public abstract SPI_W0_OFFS: number;
+  public abstract SPI_MOSI_DLEN_OFFS: number;
+  public abstract SPI_MISO_DLEN_OFFS: number;
+  public abstract TEXT_START: number;
+  public abstract ENTRY: number;
+  public abstract DATA_START: number;
+  public abstract ROM_DATA: string;
+  public abstract ROM_TEXT: string;
+
+  public get_erase_size(offset: number, size: number) {
+    return size;
+  }
+
+  public _d2h(d: number) {
+    const h = (+d).toString(16);
+    return h.length === 1 ? "0" + h : h;
+  }
+
+  public abstract get_chip_description(loader: ESPLoader): void;
+
+  public abstract get_chip_features(loader: ESPLoader): void;
+
+  public abstract get_crystal_freq(loader: ESPLoader): void;
+
+  public abstract read_mac(loader: ESPLoader): void;
+
+  public abstract _post_connect(loader: ESPLoader): void;
+
+  public abstract read_efuse(
+    loader: ESPLoader,
+    offset: number
+  ): Promise<number>;
+
+  public abstract get_pkg_version(loader: ESPLoader): void;
+  public abstract get_chip_revision(loader: ESPLoader): void;
+}
